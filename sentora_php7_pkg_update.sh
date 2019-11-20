5e810d26cbe99bcde617c102619ea8d8af43b1dc
Duke City Solutions, LLC <36783248+Dukecitysolutions@users.noreply.github.com>
2019-11-19 23:47:38 -0700

    Update sentora_php7_pkg_update.sh
    Added PHPMyAdmin 4.9 to updater

--- sentora_php7_pkg_update.sh
+++ sentora_php7_pkg_update.sh
@@ -315,15 +315,103 @@ done
 	# Start PHPmyadmin 4.9 upgrade Below - TESTING WHICH VERSION IS BEST HERE.
 	# -------------------------------------------------------------------------------
 	
-	#echo -e "\n--- Starting PHPmyadmin upgrade to 4.9..."
-	#mkdir -p /etc/sentora/panel/etc/apps/phpmyadmin-old
-	#cp -rf /etc/sentora/panel/etc/apps/phpmyadmin/* /etc/sentora/panel/etc/apps/phpmyadmin-old
-	#rm -rf /etc/sentora/panel/etc/apps/phpmyadmin/*
+	#--- Some functions used many times below
+	# Random password generator function
+	passwordgen() {
+    	l=$1
+    	[ "$l" == "" ] && l=16
+    	tr -dc A-Za-z0-9 < /dev/urandom | head -c ${l} | xargs
+	}
 	
-	# copy original conf bak to phpmyadmin
-	#cp -r /etc/sentora/panel/etc/apps/phpmyadmin-old/config.inc.php /etc/sentora/panel/etc/apps/phpmyadmin/
-	# copy new PHPmyadmin 4.9 files to PHPmyadmin dir
-	#cp -r  ~/sentora_php7_upgrade/etc/apps/phpmyadmin/* $PANEL_PATH/panel/etc/apps/phpmyadmin
+	
+	phpmyadminsecret=$(passwordgen 32);
+	
+	#echo "password"
+	#echo -e "$phpmyadminsecret"
+	
+	#Version checker function dor Mysql & PHP
+	versioncheck() { 
+		echo "$@" | gawk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; 
+	}
+
+	# Check the php version installed on the OS.
+	# phpver=php -v |grep -Eow '^PHP [^ ]+' |gawk '{ print $2 }'
+	phpver=`php -r 'echo PHP_VERSION;'`
+
+	PHPMYADMIN_OLD="/etc/sentora/panel/etc/apps/phpmyadmin_old"
+
+	if [ ! -d "$PHPMYADMIN_OLD" ] ; then		
+	
+	# Start
+	echo -e "\n-- Configuring phpMyAdmin 4.9..."
+	
+		if [[ "$(versioncheck "$phpver")" < "$(versioncheck "5.5.0")" ]]; then
+			echo -e "\n-- Your current php Version installed is $phpver, you can't upgrade phpMyAdmin to the last stable version. You need php 5.5+ for upgrade."
+		else
+			while true; do
+			read -e -p "PHPmyadmin 4.0.x will not work properly with PHP 7.3. Installer is about to upgrade PHPmyadmin to 4.9.1. Installer will backup current PHPmyadmin 4.x to /phpmyadmin_old incase something goes wrong. Do you want to keep the (O)riginal phpMyAdmin from Sentora or (U)pdate to the last stable version ? UPGRADE IS STRONGLY RECOMMENDED. (O/U)" pma
+			echo ""
+				
+				case $pma in
+        	        [Uu]* )
+            	            PHPMYADMIN_VERSION="STABLE"
+                	        cd  $PANEL_PATH/panel/etc/apps/
+							# backup 	
+                    	    mv phpmyadmin  phpmyadmin_old
+														
+							# empty folder
+							rm -rf /etc/sentora/panel/etc/apps/phpmyadmin
+
+							wget -nv -O phpmyadmin.zip https://github.com/phpmyadmin/phpmyadmin/archive/$PHPMYADMIN_VERSION.zip
+                    	    unzip -q  phpmyadmin.zip
+                        	mv phpmyadmin-$PHPMYADMIN_VERSION phpmyadmin
+											
+                        	#sed -i "s/memory_limit = .*/memory_limit = 512M/" $PHP_INI_PATH
+							#if [[ "$OS" = "CentOs" || "$OS" = "Fedora" ]]; then
+								#echo 'suhosin.executor.include.whitelist = phar' >> $PHP_EXT_PATH/suhosin.ini
+								#systemctl restart $HTTP_SERVICE
+							#fi
+							
+                        	cd phpmyadmin
+							
+                        	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+                        	php -r "if (hash_file('SHA384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+							
+							## Setup composer
+							sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+                        	#php composer-setup.php
+                        	php -r "unlink('composer-setup.php');"
+                        	composer update --no-dev
+							
+							# setup PHPmyadmin 4.9.1
+							composer create-project
+							
+                        	cd $PANEL_PATH/panel/etc/apps/
+                        	chmod -R 755 phpmyadmin
+                        	chown -R $HTTP_USER:$HTTP_USER phpmyadmin
+							rm -rf phpmyadmin/test
+                        	#rm -rf phpmyadmin.zip
+							mv $PANEL_PATH/panel/etc/apps/phpmyadmin_old/robots.txt phpmyadmin/robots.txt
+                        	#rm -rf phpmyadmin.old
+							
+							mkdir -p /etc/sentora/panel/etc/apps/phpmyadmin/tmp
+							chmod -R 777 /etc/sentora/panel/etc/apps/phpmyadmin/tmp
+							ln -s $PANEL_CONF/phpmyadmin/config.inc.php $PANEL_PATH/panel/etc/apps/phpmyadmin/config.inc.php
+							chmod 644 $PANEL_CONF/phpmyadmin/config.inc.php
+							sed -i "s|\$cfg\['blowfish_secret'\] \= '.*';|\$cfg\['blowfish_secret'\] \= '$phpmyadminsecret';|" $PANEL_CONF/phpmyadmin/config.inc.php
+	
+							# Remove phpMyAdmin's setup folder in case it was left behind
+							rm -rf $PANEL_PATH/panel/etc/apps/phpmyadmin/setup
+							
+							
+                	break;;
+                	[oO]* )
+                	break;;
+				esac
+			done
+		fi	
+		
+	fi
 	
 	
 # -------------------------------------------------------------------------------
