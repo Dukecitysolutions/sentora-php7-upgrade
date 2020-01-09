19b9060d2b03c7be6f1b73a53f0a7d2bff8c7cb0
Duke City Solutions, LLC <36783248+Dukecitysolutions@users.noreply.github.com>
2020-01-09 14:33:07 -0700

    Update sen_apache_changed.sql

--- preconf/sql/sen_apache_changed.sql
+++ preconf/sql/sen_apache_changed.sql
@@ -1,4 +1,4 @@
 USE `sentora_core`;
 
-/* Update the sentora database version number */
-UPDATE `x_settings` SET `so_value_tx` = 'true' WHERE `so_name_vc` = 'apache_changed';
+/* REMOVED Update the sentora database version number REMOVED*/
+/*UPDATE `x_settings` SET `so_value_tx` = 'true' WHERE `so_name_vc` = 'apache_changed';*/
