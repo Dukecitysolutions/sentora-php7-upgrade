90f16a8aaaca2987d20eb5976867b1d93f2315e5
Duke City Solutions, LLC <36783248+Dukecitysolutions@users.noreply.github.com>
2020-01-09 14:33:10 -0700

    Update sentora_1_0_3_1.sql

--- preconf/sql/sentora_1_0_3_1.sql
+++ preconf/sql/sentora_1_0_3_1.sql
@@ -1,7 +1,7 @@
 USE `sentora_core`;
 
-/* Update the sentora database version number */
-UPDATE `x_settings` SET `so_value_tx` = '1.0.3.1-Build 0.3.2-BETA' WHERE `so_name_vc` = 'dbversion';
+/* REMOVED Update the sentora database version number REMOVED*/
+/*UPDATE `x_settings` SET `so_value_tx` = '1.0.3.1-Build 0.3.2-BETA' WHERE `so_name_vc` = 'dbversion';*/
 
 /* Expand DNS target for DKIM sizing */
 ALTER TABLE `x_dns` CHANGE `dn_target_vc` `dn_target_vc` VARCHAR(2000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
