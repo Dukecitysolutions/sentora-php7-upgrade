bb32f96018ddd01698eb025ffea889863497db12
Duke City Solutions, LLC <36783248+Dukecitysolutions@users.noreply.github.com>
2020-02-21 20:17:33 -0700

    Update sentora_1_0_3_1.sql

--- preconf/sql/sentora_1_0_3_1.sql
+++ preconf/sql/sentora_1_0_3_1.sql
@@ -1,3 +1,4 @@
+
 USE `sentora_core`;
 
 /* Expand DNS target for DKIM sizing */
@@ -12,7 +13,7 @@ ALTER TABLE `x_vhosts` ADD COLUMN `vh_ssl_port_in` INT(6) NULL AFTER `vh_ssl_tx`
 /* add vhost vh_custom_sp_tx column after vh_ssl_port_in */
 ALTER TABLE `x_vhosts` ADD COLUMN `vh_custom_sp_tx` text NULL AFTER `vh_ssl_port_in`;
 
-/* Enter new tables to x_settings sentora database for apache_admin */
+/* Enter new tables to x_settings sentora database for apache_admin vhost SSL*/
 INSERT INTO `x_settings` ( `so_name_vc`, `so_cleanname_vc`, `so_value_tx`, `so_defvalues_tx`, `so_desc_tx`, `so_module_vc`, `so_usereditable_en` ) VALUES ('panel_ssl_tx', 'Sentora Panel SSL Config', null, null, 'Sentora SSL settings and certs', 'Sentora Config', true); 
 
 
