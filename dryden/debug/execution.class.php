0babb80d2ed7cb74403ac7aeb4c9a98fda46dcad
Duke City Solutions, LLC <36783248+Dukecitysolutions@users.noreply.github.com>
2020-09-19 11:45:30 -0600

    Updated
    Updated

---
+++ dryden/debug/execution.class.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @copyright 2014-2020 Sentora Project (http://www.sentora.org/) 
+ * Sentora is a GPL fork of the ZPanel Project whose original header follows:
+ *
+ * Provides controller/framework execution debug tools.
+ * @package zpanelx
+ * @subpackage dryden -> debug
+ * @version 1.0.0
+ * @author Bobby Allen (ballen@bobbyallen.me)
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ */
+class debug_execution {
+
+    /**
+     * Displays the current script memory usage.
+     * @author Bobby Allen (ballen@bobbyallen.me)
+     * @return string Human readable memory usage for of the script.
+     */
+    static function ScriptMemoryUsage() {
+        $mem_usage = memory_get_usage(false);
+        if ($mem_usage < 1024) {
+            $retval = $mem_usage . " bytes";
+        } elseif ($mem_usage < 1048576) {
+            $retval = round($mem_usage / 1024, 2) . " KB";
+        } else {
+            $retval = round($mem_usage / 1048576, 2) . " MB";
+        }
+        return $retval;
+    }
+
+    /**
+     * Gets a list of all the currently loaded classes.
+     * @author Bobby Allen (ballen@bobbyallen.me)
+     * @return array List of the classes that are currently loaded.
+     */
+    static function GetLoadedClasses() {
+        $classes_loaded = get_declared_classes();
+        return $classes_loaded;
+    }
+
+    /**
+     * Gets a list of all the SQL queries executed for this request.
+     * @global db_driver $zdbh The ZPX database handle.
+     * @author Bobby Allen (ballen@bobbyallen.me)
+     * @return array List of all executed SQL queries for this request.
+     */
+    static function GetSQLQueriesExecuted() {
+        global $zdbh;
+        return $zdbh->getExecutedQueries();
+    }
+
+}
+
+?>
---
+++ dryden/debug/phperrors.class.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @copyright 2014-2020 Sentora Project (http://www.sentora.org/) 
+ * Sentora is a GPL fork of the ZPanel Project whose original header follows:
+ *
+ * Development class enables PHP error reporting for ease of development!
+ * @package zpanelx
+ * @subpackage dryden -> debug
+ * @version 1.0.0
+ * @author Bobby Allen (ballen@bobbyallen.me)
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ */
+class debug_phperrors {
+
+    /**
+     * Sets PHP error reporting to ON and displays ALL errors if set to 'dev' otherwise will disable all errors.
+     * @author Bobby Allen (ballen@bobbyallen.me)
+     * @param str $mode Either 'dev' or 'prod', is left blank 'prod' is used by default.
+     */
+    static function SetMode($mode = '') {
+        if ($mode == 'dev') {
+            error_reporting('E_ALL');
+            ini_set('error_reporting', E_ALL);
+        } else {
+            error_reporting('E_NONE');
+            ini_set('error_reporting', E_NONE);
+        }
+    }
+
+}
+
+?>
